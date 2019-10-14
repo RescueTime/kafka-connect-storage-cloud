2e3f52cd856460f0e9c1465fd136da61b64967e2
Chris Tweney <chris@media.berkeley.edu>
2019-10-14 15:08:18 -0700

    First draft of our custom text shortener

---
+++ kafka-connect-s3/src/main/java/com/rescuetime/connect/transform/TextShortener.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2018 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.rescuetime.connect.transform;
+
+import jline.internal.Log;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.data.Field;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.transforms.Transformation;
+import org.apache.kafka.connect.transforms.util.SimpleConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.apache.kafka.connect.transforms.util.Requirements.requireMap;
+import static org.apache.kafka.connect.transforms.util.Requirements.requireStruct;
+
+public abstract class TextShortener<R extends ConnectRecord<R>> implements Transformation<R> {
+
+  private static final Logger log = LoggerFactory.getLogger(TextShortener.class);
+
+  private static final String PURPOSE = "string shortening";
+
+  private List<String> topics;
+
+  private List<String> fields;
+
+  TextShortener() {
+  }
+
+  interface ConfigName {
+    String TOPICS = "topics";
+    String FIELDS = "fields";
+  }
+
+  private static final ConfigDef CONFIG_DEF = new ConfigDef()
+      .define(ConfigName.TOPICS, ConfigDef.Type.LIST, Collections.emptyList(), ConfigDef.Importance.HIGH,
+          "Topics to include in shortening.")
+      .define(ConfigName.FIELDS, ConfigDef.Type.LIST, Collections.emptyList(), ConfigDef.Importance.HIGH,
+          "Fields to be shortened.");
+
+  @Override
+  public R apply(R record) {
+    log.info("Shortening record " + record.toString());
+
+    R processedRecord;
+    Schema schema = record.valueSchema();
+    if (schema == null) {
+      processedRecord = applySchemaless(record);
+    } else {
+      processedRecord = applyWithSchema(record, schema);
+    }
+    Log.info("Record after shortening: " + processedRecord.toString());
+    return processedRecord;
+  }
+
+  private R applySchemaless(R record) {
+    final Map<String, Object> value = requireMap(record.value(), PURPOSE);
+    return record.newRecord(record.topic(), record.kafkaPartition(),
+        record.keySchema(), record.key(), null,
+        updateValue(value, record.topic()),
+        record.timestamp());
+  }
+
+  private Map<String, Object> updateValue(Map<String, Object> value, String topic) {
+    final Map<String, Object> updatedValue = new HashMap<>(value.size());
+    for (Map.Entry<String, Object> e : value.entrySet()) {
+      final Object fieldValue = e.getValue();
+      final String fieldName = e.getKey();
+      if (fieldValue instanceof Map) {
+        // recurse into map structures
+        updatedValue.put(fieldName, updateValue(requireMap(fieldValue, PURPOSE), topic));
+      } else if (fieldValue instanceof String && filter(topic, fieldName)) {
+        updatedValue.put(fieldName, fieldValue.toString().substring(0, 10));
+      } else {
+        updatedValue.put(fieldName, fieldValue);
+      }
+    }
+    return updatedValue;
+  }
+
+  private boolean filter(String topic, String fieldname) {
+    return topics.contains(topic) && fields.contains(fieldname);
+  }
+
+  private R applyWithSchema(R record, Schema schema) {
+    final Struct value = requireStruct(record.value(), PURPOSE);
+
+    final Struct updatedValue = new Struct(schema);
+    for (Field field : schema.fields()) {
+      final String fieldName = field.name();
+      if (filter(record.topic(), fieldName)) {
+
+        final Object fieldValue = value.get(fieldName);
+
+        if (fieldValue instanceof String) {
+          updatedValue.put(fieldName, fieldValue.toString().substring(0, 10));
+        } else {
+          updatedValue.put(fieldName, fieldValue);
+        }
+      } else {
+        updatedValue.put(fieldName, value.get(fieldName));
+      }
+    }
+
+    return record.newRecord(record.topic(), record.kafkaPartition(),
+        record.keySchema(), record.key(), record.valueSchema(), updatedValue, record.timestamp());
+  }
+
+  @Override
+  public void close() {
+  }
+
+  @Override
+  public void configure(Map<String, ?> configs) {
+    final SimpleConfig config = new SimpleConfig(CONFIG_DEF, configs);
+    topics = config.getList(ConfigName.TOPICS);
+    fields = config.getList(ConfigName.FIELDS);
+  }
+
+  @Override
+  public ConfigDef config() {
+    return CONFIG_DEF;
+  }
+
+  static class Key<R extends ConnectRecord<R>> extends TextShortener<R> {
+
+  }
+
+  static class Value<R extends ConnectRecord<R>> extends TextShortener<R> {
+
+  }
+}
---
+++ kafka-connect-s3/src/test/java/com/rescuetime/connect/transform/TextShortenerTest.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2018 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.rescuetime.connect.transform;
+
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.sink.SinkRecord;
+import org.junit.After;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class TextShortenerTest {
+  private TextShortener<SinkRecord> xform = new TextShortener.Value<>();
+
+  @After
+  public void teardown() {
+    xform.close();
+  }
+
+  @Test
+  public void schemaless() {
+    final Map<String, String> props = new HashMap<>();
+    props.put(TextShortener.ConfigName.TOPICS, "test");
+    props.put(TextShortener.ConfigName.FIELDS, "details");
+    xform.configure(props);
+
+    final Map<String, Object> value = new HashMap<>();
+    value.put("unshortened", "blah blah blah sdlkflksdjfkljsdflkjsdajklfasdklf 03");
+    value.put("details", "details will be truncated after 10 chars");
+    value.put("count", 5);
+    value.put("empty", null);
+
+    final SinkRecord record = new SinkRecord("test", 0, null, null, null, value, 0);
+    final SinkRecord transformedRecord = xform.apply(record);
+
+    final Map updatedValue = (Map) transformedRecord.value();
+    assertEquals(4, updatedValue.size());
+    assertEquals("blah blah blah sdlkflksdjfkljsdflkjsdajklfasdklf 03", updatedValue.get("unshortened"));
+    assertEquals("details wi", updatedValue.get("details"));
+    assertEquals(5, updatedValue.get("count"));
+    assertNull(updatedValue.get("empty"));
+  }
+
+  @Test
+  public void withSchema() {
+    final Map<String, String> props = new HashMap<>();
+    props.put(TextShortener.ConfigName.TOPICS, "test");
+    props.put(TextShortener.ConfigName.FIELDS, "details");
+    xform.configure(props);
+
+    final Schema schema = SchemaBuilder.struct()
+        .field("unshortened", Schema.STRING_SCHEMA)
+        .field("details", Schema.STRING_SCHEMA)
+        .field("count", Schema.INT32_SCHEMA)
+        .field("empty", Schema.OPTIONAL_INT32_SCHEMA)
+        .build();
+
+    final Struct value = new Struct(schema);
+    value.put("unshortened", "blah blah blah sdlkflksdjfkljsdflkjsdajklfasdklf 03");
+    value.put("details", "details will be truncated after 10 chars");
+    value.put("count", 5);
+    value.put("empty", null);
+
+    final SinkRecord record = new SinkRecord("test", 0, null, null, schema, value, 0);
+    final SinkRecord transformedRecord = xform.apply(record);
+
+    final Struct updatedValue = (Struct) transformedRecord.value();
+    assertEquals(4, updatedValue.schema().fields().size());
+    assertEquals("blah blah blah sdlkflksdjfkljsdflkjsdajklfasdklf 03", updatedValue.get("unshortened"));
+    assertEquals("details wi", updatedValue.get("details"));
+    assertEquals(5, updatedValue.get("count"));
+    assertNull(updatedValue.get("empty"));
+  }
+
+  @Test
+  public void emptyMessage() {
+    final Map<String, String> props = new HashMap<>();
+    xform.configure(props);
+    final Map<String, Object> value = new HashMap<>();
+    final SinkRecord record = new SinkRecord("test", 0, null, null, null, value, 0);
+    final SinkRecord transformedRecord = xform.apply(record);
+
+    final Map updatedValue = (Map) transformedRecord.value();
+    assertTrue(updatedValue.isEmpty());
+
+  }
+
+  @Test
+  public void schemalessNestedMap() {
+    final Map<String, String> props = new HashMap<>();
+    props.put(TextShortener.ConfigName.TOPICS, "test");
+    props.put(TextShortener.ConfigName.FIELDS, "details");
+    xform.configure(props);
+
+    final Map<String, Object> value = new HashMap<>();
+    final Map<String, Object> subtree = new HashMap<>();
+    final Map<String, Object> subsubtree = new HashMap<>();
+    subtree.put("details", "details will be truncated after 10 chars");
+    subsubtree.put("details", "subsubtreedetails will be truncated after 10 chars");
+    subtree.put("subsubtree", subsubtree);
+    value.put("subtree", subtree);
+
+    final SinkRecord record = new SinkRecord("test", 0, null, null, null, value, 0);
+    final SinkRecord transformedRecord = xform.apply(record);
+
+    final Map updatedValue = (Map) transformedRecord.value();
+    assertEquals(1, updatedValue.size());
+    final Map updatedSubtree = (Map) updatedValue.get("subtree");
+    assertEquals("details wi", updatedSubtree.get("details"));
+    final Map updatedSubsubtree = (Map) updatedSubtree.get("subsubtree");
+    assertEquals("subsubtree", updatedSubsubtree.get("details"));
+  }
+
+  @Test
+  public void nestedMapWithSchema() {
+    final Map<String, String> props = new HashMap<>();
+    props.put(TextShortener.ConfigName.TOPICS, "test");
+    props.put(TextShortener.ConfigName.FIELDS, "details");
+    xform.configure(props);
+//
+//    final Schema schema = SchemaBuilder.struct()
+//        .m("subtree",
+//            SchemaBuilder.
+//            ;
+//            .)
+//        .field("details", Schema.STRING_SCHEMA)
+//        .field("count", Schema.INT32_SCHEMA)
+//        .field("empty", Schema.OPTIONAL_INT32_SCHEMA)
+//        .build();
+//
+//    final Struct value = new Struct(schema);
+//
+//    final Map<String, Object> value = new HashMap<>();
+//    final Map<String, Object> subtree = new HashMap<>();
+//    final Map<String, Object> subsubtree = new HashMap<>();
+//    subtree.put("details", "details will be truncated after 10 chars");
+//    subsubtree.put("details", "subsubtreedetails will be truncated after 10 chars");
+//    subtree.put("subsubtree", subsubtree);
+//    value.put("subtree", subtree);
+//
+//    final SinkRecord record = new SinkRecord("test", 0, null, null, null, value, 0);
+//    final SinkRecord transformedRecord = xform.apply(record);
+//
+//    final Map updatedValue = (Map) transformedRecord.value();
+//    assertEquals(1, updatedValue.size());
+//    final Map updatedSubtree = (Map) updatedValue.get("subtree");
+//    assertEquals("details wi", updatedSubtree.get("details"));
+//    final Map updatedSubsubtree = (Map) updatedSubtree.get("subsubtree");
+//    assertEquals("subsubtree", updatedSubsubtree.get("details"));
+  }
+
+}
